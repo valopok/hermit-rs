89fb96ea4acd3863b174f2403cbf52f343918794
valopok <valopok@protonmail.com>
2025-09-07 23:35:00 +0200

    Added benchmarks and corrected configuration

--- examples/nvme-test/src/main.rs
+++ examples/nvme-test/src/main.rs
@@ -8,44 +8,61 @@ use tests::run_tests;
 
 fn main() {
 	println!("Hello, NVMe!");
-	match example() {
-		Err(error) => eprintln!("{error:?}"),
-		Ok(()) => println!("Success!"),
-	}
+	// match example() {
+	// 	Err(error) => eprintln!("{error:?}"),
+	// 	Ok(()) => println!("Success!"),
+	// }
 	run_tests();
 }
 
+#[allow(dead_code)]
 fn example() -> Result<(), SysNvmeError> {
-	let number_of_namespaces = get_number_of_namespaces()?;
-	println!("Number of namespaces: {number_of_namespaces}.");
+	let namespace_ids = namespace_ids()?;
+	println!("Namespace IDs: {namespace_ids:?}.");
 
-	let max_buffer_size = get_max_buffer_size()?;
-	println!("Max buffer size: {max_buffer_size:#X}.");
+	let namespace_id = namespace_ids[0];
+	let namespace = namespace(&namespace_id)?;
+	println!("Namespace: {namespace:?}");
+	println!(
+		"Total namespace size: {}",
+		namespace.blocks * namespace.block_size
+	);
 
-	let max_number_of_queue_entries = get_max_number_of_queue_entries()?;
-	println!("Max number of queue entries: {max_number_of_queue_entries}.");
+	let maximum_transfer_size = maximum_transfer_size()?;
+	println!("Maximum transfer size: {maximum_transfer_size}.");
 
-	let namespace_index: usize = 0;
-	let size_of_namespace = get_size_of_namespace(namespace_index)?;
-	println!("Size of namespace with index {namespace_index}: {size_of_namespace:#X}.");
+	let maximum_number_of_io_queue_pairs = maximum_number_of_io_queue_pairs()?;
+	println!("Maximum number of I/O queue pairs: {maximum_number_of_io_queue_pairs}.");
 
-	let number_of_entries = 2;
-	let io_queue_pair_id = create_io_queue_pair(namespace_index, number_of_entries)?;
-	println!("Created IO queue pair with ID {io_queue_pair_id:?} and {number_of_entries} queue entries for namespace {namespace_index}.");
+	let maximum_queue_entries_supported = maximum_queue_entries_supported()?;
+	println!("Maximum queue entries supported: {maximum_queue_entries_supported}.");
 
-	let buffer_1: [u8; 16] = [
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, b'A', b'B', b'C', b'D', b'E', b'F',
-	];
+	let io_queue_pair_id = create_io_queue_pair(&namespace_id, maximum_queue_entries_supported)?;
+	println!(
+		"Created IO queue pair with ID {} and {} queue entries for namespace {}.",
+		io_queue_pair_id.0, maximum_queue_entries_supported, namespace_id.0
+	);
+
+    let length = 16;
+    let mut buffer_1 = allocate_buffer(&io_queue_pair_id, length)?;
+    for i in 0..length {
+        buffer_1[i] = i as u8;
+    }
+
 	let logical_block_address = 0;
 	write_to_io_queue_pair(&io_queue_pair_id, &buffer_1, logical_block_address)?;
 	println!("Wrote to IO queue pair with ID {io_queue_pair_id:?}.");
 
-	let mut buffer_2: [u8; 16] = [0; 16];
+    let mut buffer_2 = allocate_buffer(&io_queue_pair_id, length)?;
 	read_from_io_queue_pair(&io_queue_pair_id, &mut buffer_2, logical_block_address)?;
 	println!("Read from IO queue pair with ID {io_queue_pair_id:?}.");
 
-	println!("buffer_1: {buffer_1:?}");
-	println!("buffer_2: {buffer_2:?}");
+	println!("buffer_1: {:?}", &buffer_1[0..length]);
+	println!("buffer_2: {:?}", &buffer_2[0..length]);
+
+    deallocate_buffer(&io_queue_pair_id, buffer_1)?;
+    deallocate_buffer(&io_queue_pair_id, buffer_2)?;
+	println!("Deallocated buffers.");
 
 	delete_io_queue_pair(io_queue_pair_id)?;
 	println!("Deleted IO queue pair.");
